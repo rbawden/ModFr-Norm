8a7f1c044cbb618a3cea24b36a992413ef6a6988
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-01-23 16:19:13 +0100

    removed old mt model folder

--- mt_models/transformer/3/translate_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-thisdir=`dirname $0`
-maindir=$thisdir/../../../
-data=$maindir/data/shed/bin/bpe_joint_1000
-model=checkpoint_bestwordacc_sym.pt
-dataset=test
-
-bash $maindir/scripts/translate.sh $model $dataset $data $thisdir/$model.$dataset
-
