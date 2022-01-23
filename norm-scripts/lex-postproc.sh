c1393c6c3ae30f50c7f4de5557973087666a53b0
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-01-23 16:21:34 +0100

    added lefff to lex postproc script

--- norm-scripts/lex-postproc.sh
+++ norm-scripts/lex-postproc.sh
@@ -2,4 +2,4 @@
 
 thisdir=`dirname $0`
 
-bash $thisdir/pre-normalise.sh | perl $thisdir/alModFrNormalise.pl -l
+bash $thisdir/pre-normalise.sh | perl $thisdir/alModFrNormalise.pl -l $thisdir/../data/lexicons/lefff-3.4.mlex
