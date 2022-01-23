14fdc997a8e3381323e074e6b85caf0e3c1ddf8b
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-01-23 15:45:39 +0100

    added models

---
+++ mt-models/best-lstm/3/translate_val.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+thisdir=`dirname $0`
+maindir=`realpath $thisdir/../../../`
+valid_set=$maindir/data/bin/bpe_joint_1000
+
+[ -d $thisdir/valid_outputs ] || mkdir $thisdir/valid_outputs
+[ ! -f $thisdir/valid.eval ] || rm $thisdir/valid.eval
+
+for model in `ls -tr checkpoint*.pt`; do
+    # translate the valid set
+    if [ ! -f $thisdir/valid_outputs/$model.valid ]; then
+	bash $maindir/mt-scripts/translate.sh $model 'valid' \
+	     $valid_set $thisdir/valid_outputs/$model.valid
+    fi
+done
