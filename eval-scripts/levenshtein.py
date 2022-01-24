a8a2218a092e4b60cb682f222ebdecbed24ebad0
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-01-24 15:18:13 +0100

    last updates

--- eval-scripts/levenshtein.py
+++ eval-scripts/levenshtein.py
@@ -15,21 +15,22 @@ def levenshtein_score(sents_ref, sents_pred, align_type='ref', cache_file=None):
     for sent_ref, sent_pred in zip(sents_ref, sents_pred):
         if align_type == 'ref':
             if (sent_ref, sent_pred) in cache and 'score' in cache[(sent_ref, sent_pred)]:
-                score += cache[(sent_ref, sent_pred)]['score']
+                score += cache[(sent_ref, sent_pred)]['lev_score']
             else:
                 score += edit_distance(sent_ref, sent_pred)
                 if (sent_ref, sent_pred) not in cache:
                     cache[(sent_ref, sent_pred)] = {}
-                cache[(sent_ref, sent_pred)]['score'] = score
+                cache[(sent_ref, sent_pred)]['lev_score'] = score
             num_chars += max(len(sent_pred), len(sent_ref))
+            #print(score, num_chars, score/num_chars)
         else:
             if (sent_pred, sent_ref) in cache and 'score' in cache[(sent_pred, sent_ref)]:
-                score += cache[(sent_pred, sent_ref)]['score']
+                score += cache[(sent_pred, sent_ref)]['lev_score']
             else:
                 score += edit_distance(sent_pred, sent_ref)
                 if (sent_pred, sent_ref) not in cache:
                     cache[(sent_pred, sent_ref)] = {}
-                cache[(sent_pred, sent_ref)]['score'] = score
+                cache[(sent_pred, sent_ref)]['lev_score'] = score
             num_chars += max(len(sent_pred), len(sent_ref))
     # dump cache file
     if cache_file is not None:
@@ -43,7 +44,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('ref')
     parser.add_argument('pred')
-    parser.add_argument('-a', '--align_type', choices=('ref', 'pred'),
+    parser.add_argument('-a', '--align_type', choices=('ref', 'pred'), required=True,
                         help='Which file\'s tokenisation to use as reference for alignment.')
     parser.add_argument('-c', '--cache', help='pickle cache file to store scores/alignments', default=None)
     args = parser.parse_args()
