9177b213cc9ddfa697d26e529b8f93abbc035e81
Rachel Bawden <6254922+rbawden@users.noreply.github.com>
2022-04-20 11:45:41 +0200

    udpated missing param in eval

--- eval-scripts/eval_detailed_aux.sh
+++ eval-scripts/eval_detailed_aux.sh
@@ -21,7 +21,7 @@ fi
 # List of different evaluation scripts
 bleu_score=`bash $thisdir/bleu.sh $ref_file $hyp_file fr`
 chrf_score=`bash $thisdir/chrf.sh $ref_file $hyp_file fr`
-lev_char_score=`python $thisdir/levenshtein.py $ref_file $hyp_file $cachefile`
+lev_char_score=`python $thisdir/levenshtein.py $ref_file $hyp_file $cachefile -a ref`
 wordacc_scores=`python $thisdir/word_acc.py $ref_file $hyp_file $cache_file -a "ref,pred,both" | perl -CS -Mutf8 -pe "s/^/§$type,wordacc_r2h=/; s/ /§$type,wordacc_h2r=/; s/ /§$type,wordacc_sym=/; s/§/ /g"`
 
 printf "$type,bleu=$bleu_score $type,chrf=$chrf_score $type,lev_char=$lev_char_score$wordacc_scores"
