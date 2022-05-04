8e21e4ed7f69844791a999e3fc89f7fe38ba5243
Rachel Bawden <6254922+rbawden@users.noreply.github.com>
2022-05-04 14:47:28 +0200

    added script for over and under normalisation

--- eval-scripts/over_and_under_norm.py
+++ eval-scripts/over_and_under_norm.py
@@ -11,12 +11,9 @@ stats = {'keep': {'keep': 0, 'nokeep': 0}, 'nokeep': {'keep': 0, 'nokeep': 0}}
 # check how conservative models are
 with open(args.gold_align) as gfp, open(args.sys_align) as sfp:
     for gline, sline in zip(gfp, sfp):
-        # correction of alignment
-        gline = re.sub('[  ]>', '>', gline)
-        sline = re.sub('[  ]>', '>', sline)
 
-        gold_toks = [x for x in gline.split('\t')[-1].split() if x not in ['', '▁']]
-        sys_toks = [x for x in sline.split('\t')[-1].split() if x not in ['', '▁']]
+        gold_toks = re.split(' +', gline)
+        sys_toks = re.split(' +', sline)
         if len(gold_toks) != len(sys_toks):
             print(gline)
             print(sline)
@@ -27,14 +24,14 @@ with open(args.gold_align) as gfp, open(args.sys_align) as sfp:
         for g, s in zip(gold_toks, sys_toks):
             # if no change in gold
             gtype, stype = 'keep', 'keep'
-            if '>' in g:
+            if '|||' in g:
                 gtype = 'nokeep'
-            if '>' in s:
+            if '|||' in s:
                 stype = 'nokeep'
             stats[gtype][stype] += 1
 
 print('Correctly did not change = ', stats['keep']['keep'])
 print('% of those that should have been kept = ', stats['keep']['keep']/sum(stats['keep'].values()))
-print('Should have kept same = ', stats['keep']['nokeep'])
-print('Should have modified = ', stats['nokeep']['keep'])
+print('Should have kept same and modified = ', stats['keep']['nokeep'])
+print('Should have modified and did not = ', stats['nokeep']['keep'])
 print('Correctly did change', stats['nokeep']['nokeep'])
