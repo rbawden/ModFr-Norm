f46de086cc248e45db819c8f6b04e6bdca1960b8
Rachel Bawden <6254922+rbawden@users.noreply.github.com>
2022-05-16 15:44:20 +0200

    added script to download models

---
+++ data-scripts/download_models.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+thisdir=`dirname $0`
+maindir=`realpath $thisdir/..`
+
+[ -d $maindir/final-mt-models ] || mkdir $maindir/final-mt-models
+
+if [ ! -d $maindir/final-mt-models/lstm ]; then
+    if [ ! -f $maindir/lstm.zip ]; then
+	wget https://zenodo.org/record/6481539/files/lstm.zip?download=1
+	mv $maindir/lstm.zip?download=1 $maindir/lstm.zip
+    fi
+    unzip $maindir/lstm.zip -d $maindir/final-mt-models/
+else
+    echo "The folder $maindir/final-mt-models/lstm already exists so not redownloading. To force the download of the LSTM model, please delete this folder first."
+fi
+
+if [ ! -d $maindir/final-mt-models/smt ]; then
+    if [ ! -f $maindir/smt.zip ]; then
+	wget https://zenodo.org/record/6482368/files/smt.zip?download=1
+	mv $maindir/smt.zip?download=1 $maindir/smt.zip
+    fi
+    unzip $maindir/smt.zip -d $maindir/final-mt-models/
+else
+    echo "The folder $maindir/final-mt-models/smt already exists so not redownloading. To force the download of the SMT model, please delete this folder first."
+fi
+
+if [ ! -d $maindir/final-mt-models/transformer ]; then
+    if [ ! -f $maindir/transformer.zip ]; then
+	wget https://zenodo.org/record/6482342/files/transformer.zip?download=1
+	mv $maindir/transformer.zip?download=1 $maindir/transformer.zip
+    fi
+    unzip $maindir/transformer.zip -d $maindir/final-mt-models/
+else
+    echo "The folder $maindir/final-mt-models/transformer already exists so not redownloading. To force the download of the transformer model, please delete this folder first."
+fi
+
+
