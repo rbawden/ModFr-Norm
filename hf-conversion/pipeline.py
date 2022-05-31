c0d6341c9f6090acf6ffb698553c2233954f649c
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2022-05-31 23:22:04 +0200

    added edit distance functions to pipeline

--- hf-conversion/pipeline.py
+++ hf-conversion/pipeline.py
@@ -8,17 +8,131 @@ import unicodedata
 import sys, os
 import re
 from tqdm.auto import tqdm
+import operator
 
-class ListDataset(Dataset):
-     def __init__(self, original_list):
-          self.original_list = original_list
-          
-     def __len__(self):
-          return len(self.original_list)
-          
-     def __getitem__(self, i):
-          return self.original_list[i]
+######## Edit distance functions #######
 
+def _wedit_dist_init(len1, len2):
+    lev = []
+    for i in range(len1):
+        lev.append([0] * len2)  # initialize 2D array to zero
+    for i in range(len1):
+        lev[i][0] = i  # column 0: 0,1,2,3,4,...
+    for j in range(len2):
+        lev[0][j] = j  # row 0: 0,1,2,3,4,...
+    return lev
+
+
+def _wedit_dist_step(
+    lev, i, j, s1, s2, last_left, last_right, transpositions=False
+):
+    c1 = s1[i - 1]
+    c2 = s2[j - 1]
+
+    # skipping a character in s1
+    a = lev[i - 1][j] + _wedit_dist_deletion_cost(c1,c2)
+    # skipping a character in s2
+    b = lev[i][j - 1] + _wedit_dist_insertion_cost(c1,c2)
+    # substitution
+    c = lev[i - 1][j - 1] + (_wedit_dist_substitution_cost(c1, c2) if c1 != c2 else 0)
+
+    # pick the cheapest
+    lev[i][j] = min(a, b, c)#, d)
+
+def _wedit_dist_backtrace(lev):
+    i, j = len(lev) - 1, len(lev[0]) - 1
+    alignment = [(i, j, lev[i][j])]
+
+    while (i, j) != (0, 0):
+        directions = [
+            (i - 1, j),  # skip s1
+            (i, j - 1),  # skip s2
+            (i - 1, j - 1),  # substitution
+        ]
+
+        direction_costs = (
+            (lev[i][j] if (i >= 0 and j >= 0) else float("inf"), (i, j))
+            for i, j in directions
+        )
+        _, (i, j) = min(direction_costs, key=operator.itemgetter(0))
+
+        alignment.append((i, j, lev[i][j]))
+    return list(reversed(alignment))
+
+def _wedit_dist_substitution_cost(c1, c2):
+    if c1 == ' ' and c2 != ' ':
+        return 1000000
+    if c2 == ' ' and c1 != ' ':
+        return 30
+    for c in ",.;-!?'":
+        if c1 == c and c2 != c:
+            return 20
+        if c2 == c and c1 != c:
+            return 20
+    return 1
+
+def _wedit_dist_deletion_cost(c1, c2):
+    if c1 == ' ':
+        return 2
+    if c2 == ' ':
+        return 1000000
+    return 0.8
+
+def _wedit_dist_insertion_cost(c1, c2):
+    if c1 == ' ':
+        return 1000000
+    if c2 == ' ':
+        return 2
+    return 0.8
+
+def wedit_distance_align(s1, s2):
+    """
+    Calculate the minimum Levenshtein edit-distance based alignment
+    mapping between two strings. The alignment finds the mapping
+    from string s1 to s2 that minimizes the edit distance cost.
+    For example, mapping "rain" to "shine" would involve 2
+    substitutions, 2 matches and an insertion resulting in
+    the following mapping:
+    [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (4, 5)]
+    NB: (0, 0) is the start state without any letters associated
+    See more: https://web.stanford.edu/class/cs124/lec/med.pdf
+    In case of multiple valid minimum-distance alignments, the
+    backtrace has the following operation precedence:
+    1. Skip s1 character
+    2. Skip s2 character
+    3. Substitute s1 and s2 characters
+    The backtrace is carried out in reverse string order.
+    This function does not support transposition.
+    :param s1, s2: The strings to be aligned
+    :type s1: str
+    :type s2: str
+    :rtype: List[Tuple(int, int)]
+    """
+    # set up a 2-D array
+    len1 = len(s1)
+    len2 = len(s2)
+    lev = _wedit_dist_init(len1 + 1, len2 + 1)
+
+    # iterate over the array
+    for i in range(len1):
+        for j in range(len2):
+            _wedit_dist_step(
+                lev,
+                i + 1,
+                j + 1,
+                s1,
+                s2,
+                0,
+                0,
+                transpositions=False,
+            )
+
+    # backtrace to find alignment
+    alignment = _wedit_dist_backtrace(lev)
+    return alignment
+
+
+######## Normaliation pipeline #########
      
 class ReaccentPipeline(Pipeline):
 
@@ -170,6 +284,9 @@ class ReaccentPipeline(Pipeline):
         return result
 
 
+
+
+   
 def normalise_text(list_sents, batch_size=32, beam_size=5):
     tokeniser = AutoTokenizer.from_pretrained("rbawden/modern_french_normalisation", use_auth_token=True)
     model = AutoModelForSeq2SeqLM.from_pretrained("rbawden/modern_french_normalisation", use_auth_token=True)
